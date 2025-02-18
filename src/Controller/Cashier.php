e021556bccb6b980583fc701e13aadeaafadaec5
nelio <elio.nguingnang@ynov.com>
2025-02-18 11:57:55 +0100

    obj item cashier

---
+++ src/Controller/Cashier.php
@@ -0,0 +1,37 @@
+<?php
+    class Cashier {
+    public $pseudo;
+    private $id;
+    private $orders;
+
+    public $shoppinglist;
+
+    public function __construct($id, $pseudo, $price, $image) {
+        $this->id = $id;
+        $this->pseudo = $pseudo;
+        $this->price = $price;
+        $this->image = $image;
+    }
+
+    public function ShowDetails() {
+        echo "Marque: " . $this->id . "<br>";
+        echo "Modèle: " . $this->pseudo . "<br>";
+        echo "Année: " . $this->price . "<br>";
+        echo "Image: " . $this->image . "<br>";
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+
+    public function setPrice($price) {
+        $this->price = $price;
+    }
+}
+
+$maVoiture = new Item("Mike", "UpTempo", 800);
+$maVoiture->afficherDetails();
+
+    $maVoiture->setPrice(900);
+echo "Nouveau prix: " . $maVoiture->getPrice();
+?>
---
+++ src/Controller/item.php
@@ -0,0 +1,37 @@
+<?php
+    class Item {
+    public $brand;
+    public $modele;
+    private $price;
+
+    public $image;
+
+    public function __construct($brand, $modele, $price, $image) {
+        $this->brand = $brand;
+        $this->modele = $modele;
+        $this->price = $price;
+        $this->image = $image;
+    }
+
+    public function ShowDetails() {
+        echo "Marque: " . $this->brand . "<br>";
+        echo "Modèle: " . $this->modele . "<br>";
+        echo "Année: " . $this->price . "<br>";
+        echo "Image: " . $this->image . "<br>";
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+
+    public function setPrice($price) {
+        $this->price = $price;
+    }
+}
+
+$maVoiture = new Item("Mike", "UpTempo", 800);
+$maVoiture->afficherDetails();
+
+    $maVoiture->setPrice(900);
+echo "Nouveau prix: " . $maVoiture->getPrice();
+?>
