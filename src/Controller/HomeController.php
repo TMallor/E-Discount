a756738118933800e31cf01840722033141ecdbe
vincent <vincent.badusch@ynov.com>
2025-02-19 10:17:48 +0100

    correction_prbl_page_home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
-class HomeController extends AbstractController
+
+final class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/button-action', name: 'button_action', methods: ['GET', 'POST'])]
+    public function buttonAction(Request $request): Response
+    {
+        $action = $request->query->get('action') ?? $request->request->get('action');
+
+        if ($action === 'action1') {
+            // Gérer l'action 1
+            return new Response('Action 1 exécutée !');
+        } elseif ($action === 'action2') {
+            // Gérer l'action 2
+            return new Response('Action 2 exécutée !');
+        }
+
+        return new Response('Aucune action spécifiée.');
     }
 }
