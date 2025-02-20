dcdcbbfd3f701423ce402583bda1c01b2b4cf164
vincent <vincent.badusch@ynov.com>
2025-02-20 10:44:36 +0100

    db+panier

--- src/Controller/AddItemsController.php
+++ src/Controller/AddItemsController.php
@@ -2,16 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[IsGranted('ROLE_USER', message: 'Veuillez vous connecter pour accéder à cette page')]
 class AddItemsController extends AbstractController
 {
     #[Route('/additems', name: 'additems')]
-    public function index(): Response
+    #[IsGranted('ROLE_USER', message: 'Veuillez vous connecter pour ajouter des articles')]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         return $this->render('items/additems.html.twig');
     }
